daf5b0b6df238d979130455786ae444846b43812
zeeshan <skzeeshan3650@gmail.com>
2022-09-13 19:44:14 +0530

    commit 13-09

--- C/tempCodeRunnerFile.c
+++ C/tempCodeRunnerFile.c
@@ -1,2 +1,5 @@
-if (j == 1)
-        printf("\n");
+printf("Entered elements are:\n");
+            // for (int i = 0; i < n; i++)
+            // {
+            //     printf("Element %d: %d\n", i + 1, *(addr + i));
+            // }
--- C/workspace2.c
+++ C/workspace2.c
@@ -28,12 +28,92 @@ int main()
 float div(float x, float y) { return y / x; } */
 
 #include <stdio.h>
-int main()
+#include <conio.h>
+#include <stdlib.h>
+
+/* struct tech
 {
-    char name[30];
+    int n;
+}; */
+
+void main()
+{
+    /* char name[30];
     printf("Enter name: ");
     fgets(name, sizeof(name), stdin); // read string
     printf("Name: ");
-    puts(name); // display string
-    return 0;
+    puts(name); // display string */
+
+    // int a = 10;
+    // int x = 6;
+
+    // int c;
+    // scanf("%d", (printf("huhbhb\n"), &c));
+
+    // printf("%d\n", c);
+    // int a = 4565;
+
+    // printf("%d", scanf("%d", &a));
+    // printf("%d\n", sizeof(b));
+    // printf("%d\n", sizeof(c));
+
+    /* char c;
+    c = getch();
+    putch(c); */
+
+    int *addr, n;
+    char c;
+
+    scanf("%d", (printf("Enter number of elements: "), &n));
+
+    if (addr = (int *)calloc(n, sizeof(int)))
+    {
+        printf("Enter elements\n");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", (printf("Enter element %d: ", i + 1), addr + i));
+        }
+        printf("Elements of previous alloc:\n");
+        for (int i = 0; i < n; i++)
+        {
+            printf("Element %d: %d\n", i + 1, *(addr + i));
+        }
+
+        printf("Do you want to add more elements? if yes Enter y else n:\n");
+        scanf(" %c", &c);
+
+        if (c == 'y')
+        {
+            scanf("%d", (printf("Enter number of elements: "), &n));
+            addr = realloc(addr, n*sizeof(int));
+            printf("Enter elements\n");
+            for (int i = 0; i < n; i++)
+            {
+                scanf("%d", (printf("Enter element %d: ", i + 1), addr + i));
+            }
+            printf("Entered elements are:\n");
+            for (int i = 0; i < n; i++)
+            {
+                printf("Element %d: %d\n", i + 1, *(addr + i));
+            }
+        } else {
+            // printf("Entered elements are:\n");
+            // for (int i = 0; i < n; i++)
+            // {
+            //     printf("Element %d: %d\n", i + 1, *(addr + i));
+            // }
+        }
+        
+    } else {
+        printf("Error! memory not allocated.");
+        exit(0);
+    }
+    free(addr);
+    
+
+    // struct tech tech, *tech_ptr;
+
+    // tech_ptr = &tech;
+    // tech_ptr->n = 7;
+    // printf("%d", tech_ptr->n);
 }
