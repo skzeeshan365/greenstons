5b6a6fd9da8b47c3920d6bd62fa4f5783884475b
zeeshan <skzeeshan3650@gmail.com>
2023-01-29 19:54:05 +0530

    5

---
+++ C/C_advanced/Workspace/source/source_files/025.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int search(int data[], int n) {
+    for (size_t i = 0; i < SIZE; i++)
+    {
+        if (data[i] == n)
+        return i;
+    }
+    return -1;
+}
+
+int main() {
+    int data[SIZE] = {1, 2, 3, 4, 5, 54, 323, 2, 54, 5};
+    int n;
+
+    scanf("%d", (printf("Enter to search: "), &n));
+
+    int result = search(data, n);
+    if (result == -1)
+    printf("Element not found");
+    else
+    printf("Element found at index: %d", result);
+}
