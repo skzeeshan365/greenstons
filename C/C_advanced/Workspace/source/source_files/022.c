d7d6b3e7562fc518791cce678158c2997503ace2
zeeshan <skzeeshan3650@gmail.com>
2023-01-29 19:20:27 +0530

    2

---
+++ C/C_advanced/Workspace/source/source_files/022.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct data
+{
+    char *name;
+    int age;
+} data;
+
+int main() {
+    data user;
+    printf("Enter name: ");
+    scanf("%s", user.name);
+    printf("Enter age: ");
+    scanf("%d", &user.age);
+
+    printf("Entered data\nUsername: %s\tAge: %d", user.name, user.age);
+}
